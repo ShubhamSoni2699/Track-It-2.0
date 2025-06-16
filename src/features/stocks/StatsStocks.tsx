e562eb38396cc6c07e87ec5ecf905ffe139b2d68
Shubham <shubhamsoni2699@gmail.com>
2025-06-16 23:37:01 +0530

    privacy mode added to the stock page

--- src/features/stocks/StatsStocks.tsx
+++ src/features/stocks/StatsStocks.tsx
@@ -2,6 +2,7 @@ import { HiOutlineBriefcase, HiOutlineCurrencyRupee } from 'react-icons/hi';
 import Stat from '../dashboard/Stat';
 import { formatCurrency } from '../../utils/helpers';
 import { HiOutlineBanknotes, HiOutlinePercentBadge } from 'react-icons/hi2';
+import { usePrivacyMode } from '../../context/PrivacyModeContext';
 
 function StatsStocks({
   stocksCurrentAmount,
@@ -9,31 +10,46 @@ function StatsStocks({
   stockProfitLossToday,
   sotckProfitLossPerToday,
 }) {
+  const { isPrivacyMode } = usePrivacyMode();
   return (
     <>
       <Stat
         title="Invested Amount"
         color="blue"
         icon={<HiOutlineBriefcase />}
-        value={formatCurrency(stockInvestedAmount)}
+        value={
+          isPrivacyMode
+            ? String(Math.round(stockInvestedAmount)).replace(/./g, '*')
+            : formatCurrency(stockInvestedAmount)
+        }
       />
 
       <Stat
         title="Current Amount"
         color="indigo"
         icon={<HiOutlineCurrencyRupee />}
-        value={formatCurrency(stocksCurrentAmount)}
+        value={
+          isPrivacyMode
+            ? String(Math.round(stocksCurrentAmount)).replace(/./g, '*')
+            : formatCurrency(stocksCurrentAmount)
+        }
       />
 
       <Stat
         title={`${stockProfitLossToday >= 0 ? 'Todays Profit' : 'Todays Loss'}`}
         color={`${stockProfitLossToday >= 0 ? 'green' : 'red'}`}
         icon={<HiOutlineBanknotes />}
-        value={formatCurrency(stockProfitLossToday)}
+        value={
+          isPrivacyMode
+            ? String(Math.round(stockProfitLossToday)).replace(/./g, '*')
+            : formatCurrency(stockProfitLossToday)
+        }
       />
 
       <Stat
-        title={`${stockProfitLossToday >= 0 ? 'Todays Profit' : 'Todays Loss'} %`}
+        title={`${
+          stockProfitLossToday >= 0 ? 'Todays Profit' : 'Todays Loss'
+        } %`}
         color={`${stockProfitLossToday >= 0 ? 'green' : 'red'}`}
         icon={<HiOutlinePercentBadge />}
         value={`${sotckProfitLossPerToday} %`}
